ed7995deb8208a849673885b3142b545cb98fe1f
Adrian <adrian.afergon@gmnail.com>
2018-07-03 14:18:38 +0100

    Test name refactor

--- src/main.test.js
+++ src/main.test.js
@@ -1,26 +1,31 @@
 const calculate = require('./main');
 
 /*
-    Sumar 1 numero
-    Sumar 2 numeros
-    Sumar string vacio
+    Sumar 1 numero DONE
+    Sumar 2 numeros DONE
+    Sumar string vacio DONE
     Sumar 1 numero y una letra 
-    Sumar varios números
+    Sumar varios números 
     Sumar numero y letras juntas
 
 */
 
 describe('String calculator', () => {
-    it ('Sumar un numero', () =>{
+    it ('should sum one number', () =>{
         expect(calculate("1")).toBe(1);
     });
-     it ('Sumar una letra', () =>{
+     it ('should return 0 when one string', () =>{
         expect(calculate("a")).toBe(0);
         expect(calculate("a1a")).toBe(0);
         expect(calculate("")).toBe(0);
     });
-     it ('Sumar dos numeros', () =>{
+    it ('should sum two numbers, separeted by comma', () =>{
         expect(calculate("1,1")).toBe(2);
     });
-    
+    it ('should sum several numbers, separeted by comma', () =>{
+        expect(calculate("1,1,2")).toBe(4);
+    });
+    it ('should sum several numbers ignoring strings, separeted by comma', () =>{
+        expect(calculate("1,a,2")).toBe(3);
+    });
 })
