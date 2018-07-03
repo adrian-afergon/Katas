8850ef46ee8a19f35fa92bce63410a720a4fd68b
Adrian <adrian.afergon@gmnail.com>
2018-07-03 14:11:31 +0100

    Refactor

--- src/main.js
+++ src/main.js
@@ -1,16 +1,14 @@
 function calculate(expression) {
     const separator = ',';
     const numbers = expression.split(separator);
-    if (numbers.length > 0) {
-        if (numbers.length > 1) {
-            if (!isNaN(numbers[0]) && numbers[1]){
-                return Number(numbers[0]) + Number(numbers[1]);
-            }
-            return 0;
-        }
-        return !isNaN(numbers[0]) ? Number(numbers[0]) : 0
+
+    let result = 0;
+    for (let index =0; index < numbers.length; index++) {
+        if (!isNaN(numbers[index])) {
+            result += Number(numbers[index]);
+        }  
     }
-    return 0;
+    return result;
 }
 
 
