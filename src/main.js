4a107cdf71a9dbee1dd5b62a92d0fa97d0a95813
Adrian <adrian.afergon@gmnail.com>
2018-07-03 14:05:18 +0100

    Suma de varios numeros

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 function calculate(expression) {
-    if (!isNaN(expression)){
-        return Number(expression);
+    const separator = ',';
+    const numbers = expression.split(separator);
+    if (numbers.length > 0) {
+        if (numbers.length > 1) {
+            if (!isNaN(numbers[0]) && numbers[1]){
+                return Number(numbers[0]) + Number(numbers[1]);
+            }
+            return 0;
+        }
+        return !isNaN(numbers[0]) ? Number(numbers[0]) : 0
     }
     return 0;
-   
 }
 
 
