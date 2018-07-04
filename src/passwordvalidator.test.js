f3b88fe687d0b5fbd060ef8548c7db9436afe754
Adrian <adrian.afergon@gmnail.com>
2018-07-04 12:12:17 +0100

    red test rejects a password without a number

--- src/passwordvalidator.test.js
+++ src/passwordvalidator.test.js
@@ -8,9 +8,22 @@ describe('the password validator', () => {
     * min 1 lowercase
     */
   it('rejects a password with less than 6 chars', () => {
-    expect(isValidPassword('qed')).toEqual(false)
+    expect(isValidPassword('H0la')).toEqual(false)
   })
-  it('accepts a password with more than 6 chars', () => {
-    expect(isValidPassword('qedjhfl')).toEqual(true)
+
+  it('rejects a password without a number', () => {
+    expect(isValidPassword('qeDjhf')).toEqual(false)
+  })
+
+  xit('rejects a password without a uppercase', () => {
+    expect(isValidPassword('h0la4ll')).toEqual(false)
+  })
+
+  xit('rejects a password without a lowercase', () => {
+    expect(isValidPassword('H0LA4LL')).toEqual(false)
+  })
+
+  it('accepts a password', () => {
+    expect(isValidPassword('qeDjhf6')).toEqual(true)
   })
 })
