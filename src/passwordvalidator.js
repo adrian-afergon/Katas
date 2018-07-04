1f1691120638349075aa5d6c0014b95b6e1280ea
Adrian <adrian.afergon@gmnail.com>
2018-07-04 12:16:08 +0100

    green test rejects a password without a uppercase

--- src/passwordvalidator.js
+++ src/passwordvalidator.js
@@ -3,7 +3,7 @@
  * @param {string} password 
  */
 function isValidPassword(password) {
-    return (password.length >= 6) && /\d/.test(password);
+    return (password.length >= 6) && /\d/.test(password) && /[A-Z]/.test(password);
 }
   
 module.exports = isValidPassword
