59a6dd39c2c4d87f76ba0c2c3ab60152cc6442ec
Adrian <adrian.afergon@gmnail.com>
2018-07-04 12:21:48 +0100

    refactor code

--- src/passwordvalidator.js
+++ src/passwordvalidator.js
@@ -3,7 +3,23 @@
  * @param {string} password 
  */
 function isValidPassword(password) {
-    return (password.length >= 6) && /\d/.test(password) && /[A-Z]/.test(password) && /[a-z]/.test(password);
+    return isLengthGreaterThan(password, 6) && containsDigit(password) && containsUpperCase(password) && containsLowerCase(password);
+}
+
+function isLengthGreaterThan(text, number) {
+    return text.length >= number;
+}
+
+function containsDigit(text) {
+    return /\d/.test(text);
+}
+
+function containsUpperCase (text) {
+    return /[A-Z]/.test(text)
+}
+
+function containsLowerCase (text) {
+    return /[a-z]/.test(text)    
 }
   
 module.exports = isValidPassword
