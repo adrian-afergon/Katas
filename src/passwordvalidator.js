79446efc6bfe8ec466aa1c92b819d8510bd6180c
Adrian <adrian.afergon@gmnail.com>
2018-07-04 12:14:34 +0100

    green test rejects a password without a number

--- src/passwordvalidator.js
+++ src/passwordvalidator.js
@@ -3,7 +3,7 @@
  * @param {string} password 
  */
 function isValidPassword(password) {
-    return (password.length >= 6);
+    return (password.length >= 6) && /\d/.test(password);
 }
   
 module.exports = isValidPassword
